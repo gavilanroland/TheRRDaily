3be3135192af98867878851f893b6e2a37e1e459
gavilanroland <gavilanrolando@protonmail.com>
2020-11-06 21:42:10 -0300

    including code to valide form

--- js/form2.js
+++ js/form2.js
@@ -1,11 +1,11 @@
-const form = document.getElementById("form");
-const inputs = document.querySelectorAll("#form input");
+const form1 = document.getElementById("form1");
+const inputs = document.querySelectorAll("#form1 input");
 
-const xpressions = {
-    name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, 
-    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+const sections = {
+    name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+    email:/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
     password: /^.{4,12}$/, 
-	age: /^\d{18,99}$/ ,
+	age: /^\d{2,18}$/ ,
     telephone: /^\d{7,14}$/,
     address: /^[a-zA-Z0-9-.]+$/,
     city: /^[a-zA-Z0-9-.]+$/,
@@ -23,73 +23,69 @@ const camps = {
     postcode: false,
     din: false
 }
+
+
 const validForm = (e) => {
 	switch (e.target.name) {
-		case "name":
-			validcamp(xpressions.name, e.target, "name");
-		break;
-		case "email":
-			validcamp(xpressions.email, e.target, "email");
-		break;
-		case "password":
-			validcamp(xpressions.password, e.target, "password");
-		break;
-		case "age":
-			validcamp(xpressions.age, e.target, "age");
-		break;
-		case "telephone":
-			validcamp(xpressions.telephone, e.target, "telephone");
+        case "name" :
+            validcamp(sections.name, e.target, 'name');
         break;
-        case "address":
-			validcamp(xpressions.address, e.target, "address");
-		break;
-		case "city":
-			validcamp(xpressions.city, e.target, "city");
-		break;
-		case "postcode":
-			validcamp(xpressions.postcode, e.target, "postcode");
+        case "email" :
+            validcamp(sections.email, e.target, 'email');
         break;
-        case "din":
-			validcamp(xpressions.din, e.target, "din");
+        case "password" :
+            validcamp(sections.password, e.target, 'password');
         break;
-	}
-}
-const validcamp = (xpresion, input, camp) => {
-	if(xpresion.test(input.value)){
-		document.getElementById(`group__${camp}`).classList.remove("form__group-incorrect");
-		document.getElementById(`group__${camp}`).classList.add("form__group-correct");
-		document.querySelector(`#group__${camp} i`).classList.add("fa-check-circle");
-		document.querySelector(`#group__${camp} .form__input-error`).classList.remove("form__input-error-active");
-		camps[camp] = true;
-	} else {
-		document.getElementById(`group__${camp}`).classList.add("form__group-incorrect");
-		document.getElementById(`group__${camp}`).classList.remove("form__group-correct");
-		document.querySelector(`#group__${camp} i`).classList.add("fa-times-circle");
-		document.querySelector(`#group__${camp} .form__input-error`).classList.add("form__input-error-active");
-		camps[camp] = false;
-	}
-}
+        case "age" :
+            validcamp(sections.age, e.target, 'age');
+        break;
+        case "telephone" :
+            validcamp(sections.telephone, e.target, 'telephone');
+        break;
+        case "address" :
+            validcamp(sections.address, e.target, 'address');
+        break;
+        case "city" :
+            validcamp(sections.city, e.target, 'city');
+        break;
+        case "postcode" :
+            validcamp(sections.postcode, e.target, 'postcode');
+        break;
+        case "din" :
+            validcamp(sections.din, e.target, 'din');
+        break;
+
+    }}
+    const validcamp = (val1, input, camp) => {
+        if(val1.test(input.value)){
+            document.getElementById(`group__${camp}`).classList.remove("form__group-incorrect");
+            document.getElementById(`group__${camp}`).classList.add("form__group-correct");
+            document.querySelector(`#group__${camp} .form__input-error`).classList.remove("form__input-error-active");
+            camps[camp] = true;
+        } else {
+            document.getElementById(`group__${camp}`).classList.add("form__group-incorrect");
+            document.getElementById(`group__${camp}`).classList.remove("form__group-correct");
+            document.querySelector(`#group__${camp} .form__input-error`).classList.add("form__input-error-active");
+            camps[camp] = false;
+        }
+    }
+
 inputs.forEach((input) => {
-	input.addEventListener("keyup", validFormulary);
-	input.addEventListener("blur", validFormulary);
-});
+	input.addEventListener("keyup", validForm);
+	input.addEventListener("blur", validForm);
+})
 
-form.addEventListener("submit", (e) => {
+form1.addEventListener("submit", (e) => {
 	e.preventDefault();
+    if(camps.name && camps.email && camps.password && camps.age && camps.telephone && camps.address && camps.postcode && camps.city && camps.din ){
+    form1.reset();
 
-	
-	if(camps.name && camps.email && camps.password && camps.age && camps.telephone && camps.address && camps.postcode && camps.city && camps.din &&){
-		form.reset();
+    document.getElementById("form__message-succes").classList.add("form__message-succes-active");
+    setTimeout(() => {
+        document.getElementById("form__message-succes").classList.remove("form__message-succes-active");
+    }, 5000);
 
-		document.getElementById("form__message-succes").classList.add("form__menssage-succes-active");
-		setTimeout(() => {
-			document.getElementById("form__message-succes").classList.remove("form__message-succes-active");
-		}, 5000);
-
-		document.querySelectorAll(".form__group-correct").forEach((icon) => {
-			icon.classList.remove("form__group-correct");
-		});
-	} else {
-		document.getElementById("form__message").classList.add("form__message-active");
-	}
-});
+} else {
+    document.getElementById("form__message").classList.add("form__message-active");
+}
+})
